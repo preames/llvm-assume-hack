10fbd758c5444a5288e2daf785aa608601fdfbf1
Philip Reames <reames@eecs.berkeley.edu>
2014-02-25 22:09:54 -0800

    Adding some examples to show where this approach succeeds and fails.

--- assume.cpp
+++ assume.cpp
@@ -6,7 +6,7 @@ extern "C" void llvm_assume_trap(void) __attribute__ ((noreturn));
 
 #define ASSUME(e) if(!(e)) { llvm_assume_trap(); } else {}
 
-//should always return true
+/// This simple example shows how a user provided assumption can simplify control flow.  Here, this function unconditionally returns true.
 bool foo(int a) {
   ASSUME(a > 10)
 
@@ -18,16 +18,45 @@ bool foo(int a) {
 }
 
 struct A {
+  int a;
+  int b;
+
   int x;
   int y;
   int z;
+
+  inline bool invariant() {
+    return a == b && x <= y && y <= z;
+  }
 };
-  
-void bar(A& a) {
-  ASSUME( a.x <= a.y && a.y <= a.z && "precondition" );
-  // Note: what can be between here is weaker than I'd like
-  a.x = a.y;
+
+/// This is an example of a read only routine.  As we'd expect, there is no conditional due to the assert. 
+int read_only(A& a) {
+  ASSUME( a.invariant() && "precondition" );
+  int rval = a.x-a.y+a.z;
+  assert( a.invariant() && "post condition" );
+  return rval;
+}
+
+/// This example shows a limited update to the state of the type which still does not require any runtime checking.
+void limited_update(A& a) {
+  ASSUME( a.invariant() && "precondition" );
+  a.z = a.y;
+  assert( a.invariant() && "post condition" );
+}
+
+/// In this example, we can't avoid the assert, but we are able to only check part of the condition.  The part not involving 'x <= y' can be optimized away.  With a more expensive invariant, this could be significant.
+void partial_check(A& a) {
+  ASSUME( a.x > 0 && a.invariant() && "precondition" );
+  a.x--;
   a.y = a.z;
-  assert( a.x <= a.y && a.y <= a.z && "post condition" );
+  assert( a.invariant() && "post condition" );
+}
+
+/// Just for comparison sake, here's the full check from the previous example.  Evwen here, the optimizer is able to remove one of the checks, but - in particular - not the ones involving 'a' and 'b' which are completely unmodified in this function.
+void full_check(A& a) {
+  a.x--;
+  a.y = a.z;
+  assert( a.invariant() && "post condition" );
 }
 
